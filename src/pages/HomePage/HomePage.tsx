805da25798b9dd004ee83beda5e37fdf49dee2bb
Johannes Riedmüller <riedmueller.jo@gmail.com>
2025-07-04 17:58:56 +0200

    finish homepage content text

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,7 @@ import { DividerHeadline } from "../../components/DividerHeadline/DividerHeadlin
 import { MessageCard } from "../../components/MessageCard/MessageCard";
 import { PageLayoutWrapper } from "../../layouts/PageLayoutWrapper/PageLayoutWrapper";
 import styles from "./HomePage.module.css";
+import { Link } from "react-router";
 
 export const HomePage = () => {
   return (
@@ -25,15 +26,25 @@ export const HomePage = () => {
             <section>
               <DividerHeadline>Message Templates</DividerHeadline>
               <p>
-                <b>{"<"}</b>Available as a <b>Figma file{"/>"}</b>
+                <b>{"<"}</b>Available as a <b>Figma file</b> as a Free Resource
+                <b>{"/>"}</b>
               </p>
             </section>
           </div>
 
           <MessageCard>
-            Hi! I’m Johannes Riedmüller. What I built here is the practical part
-            of my bachelor thesis. The goal of these educational cards is to
-            make UI Design and Development work better together.
+            Hi! I’m Johannes Riedmüller . What I built here is the practical
+            part of my <b>bachelor thesis</b>. The goal of these educational
+            cards is to make UI Design and Development work better together!{" "}
+            <br />I hope they can help you and your collegues and boost
+            collaboration. Any feedback is appreciated, just drop me a message
+            on{" "}
+            <Link
+              className={styles.linkedInLink}
+              to="https://www.linkedin.com/in/johannes-riedm%C3%BCller-87a643317/"
+            >
+              LinkedIn!
+            </Link>
           </MessageCard>
         </div>
       </div>
